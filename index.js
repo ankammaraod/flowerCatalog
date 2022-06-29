eaf3a9319d00d0a64f46d7f91bcee83f66e0da70
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-06-29 12:39:05 +0530

    added main function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const { startServer } = require('./src/server/server.js');
 const { initializePathsAndHandlers } = require('./src/app.js');
 
-startServer(9000, initializePathsAndHandlers());
+const main = () => {
+  startServer(9000, initializePathsAndHandlers());
+};
+
+module.exports = { main };
