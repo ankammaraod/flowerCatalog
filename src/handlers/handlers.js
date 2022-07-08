9f273ed959ef425d7d622d18b37cb793c3095b0f
Ankammarao D <99481377+ankammaraod@users.noreply.github.com>
2022-07-08 13:09:59 +0530

    removed the timeout handler

--- src/handlers/handlers.js
+++ src/handlers/handlers.js
@@ -6,7 +6,6 @@ const { createRouter } = require('../server/createRouter.js');
 const { logRequestHandler } = require('./logRequestHandler.js');
 const { urlHandler } = require('./urlHandler.js');
 const { handleApiRouter } = require('./handleApi.js');
-const { timeoutHandler } = require('./timeoutHandler.js');
 const { serveAsyncFileHandler } = require('./serveAsyncFileHandler.js');
 const { readBodyParams } = require('./readBodyParamsHandler.js');
 const { injectCookie } = require('./injectCookieHandler.js')
